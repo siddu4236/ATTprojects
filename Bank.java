7becdef7a442cfde40ad4f62a4ac012ad61d9617
Jampula Siddhartha <134194635+siddu4236@users.noreply.github.com>
2024-01-13 13:21:35 +0530

    3

---
+++ Bank.java
@@ -0,0 +1,21 @@
+package Harshith;
+import java.util.HashMap;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+public class Week4 {
+	public static void main(String[] args) {
+		HashMap<String, Object>prefs=new HashMap<String, Object>();
+		prefs.put("profile.default_content_setting_values.notifications",1);
+		ChromeOptions options=new ChromeOptions();
+		options.setExperimentalOption("prefs", prefs);
+		System.setProperty("webdriver.chrome.driver&quot","D:\\DeekshithATT\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("https://www.axisbank.com/");
+		WebElement pop=driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/span ,"));
+		pop.click();
+	}
+}
